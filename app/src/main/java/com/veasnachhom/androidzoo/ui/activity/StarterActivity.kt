802b1c3caead6257a8b4c0f9eb768d1b9ba88c9f
VeasnaChhom <chhomveasna.cubc@gmail.com>
2023-07-27 14:21:28 +0700

    [View] Apply splash screen API

--- app/src/main/java/com/veasnachhom/androidzoo/ui/activity/StarterActivity.kt
+++ app/src/main/java/com/veasnachhom/androidzoo/ui/activity/StarterActivity.kt
@@ -3,12 +3,14 @@ package com.veasnachhom.androidzoo.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.veasnachhom.androidzoo.R
 import com.veasnachhom.androidzoo.attractionPlace.ui.activity.HomeActivity
 
 class StarterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_starter)
         startActivity(Intent(this, HomeActivity::class.java))
